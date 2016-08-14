b6228ad559b22b3b5fcc5d1fda0e73b13fe543d1
lolkaenot <lolkaenot@gmail.com>
2016-08-14 23:51:07 +0300

    init

---
+++ console/controllers/RbacController.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: YOBA
+ * Date: 14.08.2016
+ * Time: 19:02
+ */
+
+namespace console\controllers;
+
+
+use common\rbac\AuthorRule;
+use yii\console\Controller;
+
+class RbacController extends Controller
+{
+    public function init()
+    {
+        $auth = \Yii::$app->authManager;
+        $auth->removeAll();
+
+        $authorRule = new AuthorRule();
+        $auth->add($authorRule);
+
+        $createNews = $auth->createPermission('createNews');
+        $createNews->description = 'Создание новости';
+        $auth->add($createNews);
+
+        $updateOwnNews = $auth->createPermission('updateOwnNews');
+        $updateOwnNews->description = 'Обноволение собственной новости';
+        $updateOwnNews->ruleName = $authorRule->name;
+        $auth->add($updateOwnNews);
+
+        $updateNews = $auth->createPermission('updateNews');
+        $updateNews->description = 'Обновление новости';
+        $auth->add($updateNews);
+
+        $deleteNews = $auth->createPermission('deleteNews');
+        $deleteNews->description = 'Удаление новости';
+        $auth->add($deleteNews);
+
+        $user = $auth->createRole('user');
+        $auth->add($user);
+        $auth->addChild($user, $createNews);
+        $auth->addChild($user, $updateOwnNews);
+
+        $editor = $auth->createRole('editor');
+        $auth->add($editor);
+        $auth->addChild($editor, $user);
+        $auth->addChild($editor, $updateNews);
+
+        $admin = $auth->createRole('admin');
+        $auth->add($admin);
+        $auth->addChild($admin, $editor);
+        $auth->addChild($admin, $deleteNews);
+
+        $auth->assign($admin, 1);
+        $auth->assign($editor, 2);
+        $auth->assign($user, 3);
+    }
+
+}
---
+++ console/migrations/m160814_165506_create_user_teble.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\db\Migration;
+
+class m160814_165506_create_user_teble extends Migration
+{
+    public function up()
+    {
+        $this->createTable('user', [
+            'id' => $this->primaryKey(),
+            'username' => $this->string()->notNull()->unique(),
+            'password' => $this->string()->notNull(),
+            'email' => $this->string()->notNull()->unique(),
+            'blocked' => $this->smallInteger()->notNull()->defaultValue(0),
+            'created_at' => $this->integer()->notNull(),
+            'updated_at' => $this->integer()->notNull(),
+        ]);
+    }
+
+    public function down()
+    {
+        $this->dropTable('user');
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
